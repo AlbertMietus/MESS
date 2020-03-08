3abdb3221f5d3f19acc649ebef15373b78d63e8a
Albert Mietus <albert AT mietus DOT nl>
2020-03-08 13:30:48 +0100

    Added python+java (and generic) use-examples
    --HG--
    branch : dev-DesignWorkshop

---
+++ SoftwareCompetence/DesignWorkShops/TPE/intro/examples/TPE_demo_2.java
@@ -0,0 +1,62 @@
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.ExecutionException;
+
+public class TPE_demo_2 {
+
+    private static final int TRIES     = 10;
+    private static final int WORKERS   = 4;
+
+    public static void main(String[] args) {
+        demo();
+    }
+
+    private static void demo() {
+        ThreadPoolExecutor workers = (ThreadPoolExecutor) Executors.newFixedThreadPool(WORKERS); //* (1)
+        List<Future<Long>> resultList = new ArrayList<>();
+
+        //* (2) Sumbit all task, with a random input
+        for (int i = 1; i <= TRIES; i++) {
+            Future<Long> future = workers.submit(new aTask((long) (Math.random() * 10)));
+            resultList.add(future); // And save future result
+        }
+
+        //* (3) Now print all results; wait on the when needed [.get() does]
+        for(Future<Long> f : resultList) {
+            try {
+                System.out.println("Future done (Y/N)? :" +f.isDone() + ".\tResult is: " + f.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //* Stop the workers
+        workers.shutdown();
+    }
+}
+
+
+class aTask implements Callable {
+    Long number;
+
+    public aTask(Long number) {
+        this.number = number;                      // JAVA: save the input of this "function
+    }
+
+
+    public Long call() {
+        try {
+            TimeUnit.SECONDS.sleep(this.number/4);  // Simulate a complicated calculations ...
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return this.number * this.number;           //  ... returning the square
+    }
+}
+
